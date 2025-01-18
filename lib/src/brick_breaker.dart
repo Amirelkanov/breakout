3dab1e52e8adb8f0b21777ef6834312354053ee3
AmEl <amirelkanov0504@gmail.com>
2025-01-18 19:33:24 +0300

    BALL MOVES AND COLLISION IS GOOD!

---
+++ lib/src/brick_breaker.dart
@@ -0,0 +1,67 @@
+import 'dart:async';
+import 'dart:math' as math;
+import 'dart:ui';
+
+import 'package:flame/components.dart';
+import 'package:flame/events.dart';
+import 'package:flame/game.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+
+import 'components/components.dart';
+import 'config.dart';
+
+class BrickBreaker extends FlameGame
+    with HasCollisionDetection, KeyboardEvents {
+  BrickBreaker()
+      : super(
+          camera: CameraComponent.withFixedResolution(
+            width: gameWidth,
+            height: gameHeight,
+          ),
+        );
+
+  final rand = math.Random();
+
+  double get width => size.x;
+
+  double get height => size.y;
+
+  @override
+  FutureOr<void> onLoad() async {
+    super.onLoad();
+
+    camera.viewfinder.anchor =
+        Anchor.topLeft; // Начало координат с верхнего левого угла
+
+    world.add(PlayArea());
+
+    world.add(Ball(
+        radius: ballRadius,
+        position: size / 2,
+        velocity: Vector2((rand.nextDouble() - 0.5) * width, height * 0.2)
+            .normalized() // This keeps the speed of the ball consistent no matter which direction the ball goes
+          // ball's velocity is then scaled up to be a 1/4 of the height of the game
+          ..scale(height / 4)));
+
+    world.add(Bat(
+        size: Vector2(batWidth, batHeight),
+        cornerRadius: const Radius.circular(ballRadius / 2),
+        position: Vector2(width / 2, height * 0.95)));
+
+    debugMode = true;
+  }
+
+  @override // Add from here...
+  KeyEventResult onKeyEvent(
+      KeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+    super.onKeyEvent(event, keysPressed);
+    switch (event.logicalKey) {
+      case LogicalKeyboardKey.arrowLeft:
+        world.children.query<Bat>().first.moveBy(-batStep);
+      case LogicalKeyboardKey.arrowRight:
+        world.children.query<Bat>().first.moveBy(batStep);
+    }
+    return KeyEventResult.handled;
+  }
+}
